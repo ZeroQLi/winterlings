ae02b9497b18d234dbb0e3d87c55b096b343d892
ZeroQLi <itsrandomboy77@gmail.com>
2023-01-07 23:38:38 +0400

    chonky initial commit

---
+++ src/components/card.svelte
@@ -0,0 +1,18 @@
+<script>
+    import '../styles/main.css'
+    export let Name, Author, Summary, Link;
+</script>
+
+<div class="card hover:scale-125 transition ease-in-out">
+    <a href={Link}>
+    <h1 class=" text-4xl font-bold">{Name}</h1>
+    <h1 class=" text-3xl font-italics ">{Author}</h1>
+    {#if Summary}
+        <p>{Summary}</p>
+    {/if}
+    </a>
+</div>
+
+<style>
+
+</style>
