3522f6ee98c5c7cea87fa06d9a8d7c549228b4fb
ZeroQLi <itsrandomboy77@gmail.com>
2023-01-27 19:28:35 +0400

    update

---
+++ src/components/search.svelte
@@ -0,0 +1,30 @@
+<script>
+import Fuse from 'fuse.js'
+export let searchList;
+
+const options = {
+	keys: ['frontmatter.name', 'frontmatter.project'],
+	includeMatches: true,
+	minMatchCharLength: 2,
+	threshold: 0.5,
+};
+
+let data = searchList
+
+let query =''
+
+const fuse = new Fuse(data, options);
+
+$: posts = fuse
+    .search(query)
+    .map((res) => res.frontmatter)
+</script>
+
+<div>
+    <input class="" bind:value={query} />
+    <br />
+    <button class="" on:click={() => (query = "")}>Clear</button>
+
+</div>
+
+<style></style>
